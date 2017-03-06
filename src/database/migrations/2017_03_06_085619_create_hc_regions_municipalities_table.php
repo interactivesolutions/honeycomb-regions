da9120ae783977db838360f69c3cd420ee5bf106
Mantas Gelčius <manciusg@gmail.com>
2017-03-06 13:59:10 +0200

    Added municipalities migrations

---
+++ src/database/migrations/2017_03_06_085619_create_hc_regions_municipalities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateHcRegionsMunicipalitiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('hc_regions_municipalities', function(Blueprint $table)
+		{
+			$table->integer('count', true);
+            $table->string('id', 36)->unique('id_UNIQUE');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('country_id', 36)->index('fk_hc_regions_municipalities_hc_regions_countries1_idx');
+			$table->string('name');
+			$table->string('translation_key')->nullable();
+			$table->unique(['id','country_id'], 'id_country_UNIQUE');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('hc_regions_municipalities');
+	}
+
+}
---
+++ src/database/migrations/2017_03_06_085621_add_foreign_keys_to_hc_regions_municipalities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToHcRegionsMunicipalitiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('hc_regions_municipalities', function(Blueprint $table)
+		{
+			$table->foreign('country_id', 'fk_hc_regions_municipalities_hc_regions_countries1')->references('id')->on('hc_regions_countries')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('hc_regions_municipalities', function(Blueprint $table)
+		{
+			$table->dropForeign('fk_hc_regions_municipalities_hc_regions_countries1');
+		});
+	}
+
+}
