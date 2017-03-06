c8db97138b2d48ffc3c04e96777e1f3ec5c1bc98
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-06 10:38:09 +0200

    Updated files

---
+++ src/app/http/controllers/regions/HCCountriesController.php
@@ -0,0 +1,182 @@
+<?php namespace interactivesolutions\honeycombregions\app\http\controllers\regions;
+
+use Illuminate\Database\Eloquent\Builder;
+use interactivesolutions\honeycombcore\http\controllers\HCBaseController;
+use interactivesolutions\honeycombregions\app\models\regions\HCCountries;
+use interactivesolutions\honeycombregions\app\validators\regions\HCCountriesValidator;
+
+class HCCountriesController extends HCBaseController
+{
+
+    /**
+     * Returning configured admin view
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function adminView()
+    {
+        $config = [
+            'title'       => trans('HCRegions::regions_countries.page_title'),
+            'listURL'     => route('admin.api.regions.countries'),
+            'newFormUrl'  => route('admin.api.form-manager', ['regions-countries-new']),
+            'editFormUrl' => route('admin.api.form-manager', ['regions-countries-edit']),
+            'imagesUrl'   => route('resource.get', ['/']),
+            'headers'     => $this->getAdminListHeader(),
+        ];
+
+        if ($this->user()->can('interactivesolutions_honeycomb_regions_regions_countries_update'))
+            $config['actions'][] = 'update';
+
+        $config['actions'][] = 'search';
+
+        return view('HCCoreUI::admin.content.list', ['config' => $config]);
+    }
+
+    /**
+     * Creating Admin List Header based on Main Table
+     *
+     * @return array
+     */
+    public function getAdminListHeader()
+    {
+        return [
+            'region_id'         => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.region_id'),
+            ],
+            'common_name'       => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.common_name'),
+            ],
+            'official_name'     => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.official_name'),
+            ],
+            'translation'   => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.translation'),
+            ],
+            'iso_3166_1_alpha2' => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.iso_3166_1_alpha2'),
+            ],
+            'iso_3166_1_alpha3' => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.iso_3166_1_alpha3'),
+            ],
+            'flag_id'           => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.flag_id'),
+            ],
+            'geo_data'          => [
+                "type"  => "text",
+                "label" => trans('HCRegions::regions_countries.geo_data'),
+            ],
+
+        ];
+    }
+
+    /**
+     * Updates existing item based on ID
+     *
+     * @param $id
+     * @return mixed
+     */
+    protected function __update(string $id)
+    {
+        $record = HCCountries::findOrFail($id);
+
+        $data = $this->getInputData();
+
+        $record->update(array_get($data, 'record'));
+
+        return $this->getSingleRecord($record->id);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function listData()
+    {
+        $with = [];
+        $select = HCCountries::getFillableFields();
+
+        $list = HCCountries::with($with)->select($select)
+            // add filters
+            ->where(function ($query) use ($select) {
+                $query = $this->getRequestParameters($query, $select);
+            });
+
+        // enabling check for deleted
+        $list = $this->checkForDeleted($list);
+
+        // add search items
+        $list = $this->listSearch($list);
+
+        // ordering data
+        $list = $this->orderData($list, $select);
+
+        return $list->paginate($this->recordsPerPage)->toArray();
+    }
+
+    /**
+     * List search elements
+     * @param $list
+     * @return mixed
+     */
+    protected function listSearch(Builder $list)
+    {
+        if (request()->has('q')) {
+            $parameter = request()->input('q');
+
+            $list = $list->where(function ($query) use ($parameter) {
+                $query->where('region_id', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('common_name', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('official_name', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('translation_key', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('iso_3166_1_alpha2', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('iso_3166_1_alpha3', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('flag_id', 'LIKE', '%' . $parameter . '%')
+                    ->orWhere('geo_data', 'LIKE', '%' . $parameter . '%');
+            });
+        }
+
+        return $list;
+    }
+
+    /**
+     * Getting user data on POST call
+     *
+     * @return mixed
+     */
+    protected function getInputData()
+    {
+        (new HCCountriesValidator())->validateForm();
+
+        $_data = request()->all();
+
+        array_set($data, 'languages', array_get($_data, 'languages'));
+
+        return $data;
+    }
+
+    /**
+     * Getting single record
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getSingleRecord(string $id)
+    {
+        $with = [];
+
+        $select = HCCountries::getFillableFields();
+
+        $record = HCCountries::with($with)
+            ->select($select)
+            ->where('id', $id)
+            ->firstOrFail();
+
+        return $record;
+    }
+}
--- src/app/routes/routes.regions.countries.php
+++ src/app/routes/routes.regions.countries.php
@@ -1,22 +1,5 @@
 <?php
 
-//./packages/interactivesolutions/honeycomb-regions/src/app/routes/routes.regions.continents.php
-
-Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
-{
-    Route::get('regions/continents', ['as' => 'admin.regions.continents', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_continents_list'], 'uses' => 'regions\\HCContinentsController@adminView']);
-
-    Route::group(['prefix' => 'api'], function ()
-    {
-        Route::get('regions/continents', ['as' => 'admin.api.regions.continents', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_continents_list'], 'uses' => 'regions\\HCContinentsController@listData']);
-        Route::get('regions/continents/search', ['as' => 'admin.api.regions.continents.search', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_continents_list'], 'uses' => 'regions\\HCContinentsController@search']);
-        Route::get('regions/continents/{id}', ['as' => 'admin.api.regions.continents.single', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_continents_list'], 'uses' => 'regions\\HCContinentsController@getSingleRecord']);
-    });
-});
-
-
-//./packages/interactivesolutions/honeycomb-regions/src/app/routes/routes.regions.countries.php
-
 Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
 {
     Route::get('regions/countries', ['as' => 'admin.regions.countries', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_countries_list'], 'uses' => 'regions\\HCCountriesController@adminView']);
@@ -37,4 +20,3 @@ Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
         Route::delete('regions/countries/force', ['as' => 'admin.api.regions.countries.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_regions_regions_countries_force_delete'], 'uses' => 'regions\\HCCountriesController@forceDelete']);
     });
 });
-
