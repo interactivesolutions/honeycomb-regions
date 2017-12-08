7ed6f3efaebc14520f9ec7baea1e8e84e6110de7
vytautas <vytautas.rimeikis@gmail.com>
2017-12-08 12:13:16 +0200

    composer update

--- src/app/providers/HCRegionsServiceProvider.php
+++ src/app/providers/HCRegionsServiceProvider.php
@@ -1,19 +1,56 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace interactivesolutions\honeycombregions\app\providers;
 
 
 use InteractiveSolutions\HoneycombCore\Providers\HCBaseServiceProvider;
+use interactivesolutions\honeycombregions\app\Repositories\HCCityRepository;
 
+/**
+ * Class HCRegionsServiceProvider
+ * @package interactivesolutions\honeycombregions\app\providers
+ */
 class HCRegionsServiceProvider extends HCBaseServiceProvider
 {
+    /**
+     * @var string
+     */
     protected $homeDirectory = __DIR__;
 
+    /**
+     * @var array
+     */
     protected $commands = [];
 
+    /**
+     * @var string
+     */
     protected $namespace = 'interactivesolutions\honeycombregions\app\http\controllers';
 
+    /**
+     * @var string
+     */
     public $serviceProviderNameSpace = 'HCRegions';
+
+    /**
+     *
+     */
+    public function register()
+    {
+        $this->registerRepositories();
+
+        parent::register();
+    }
+
+    /**
+     *
+     */
+    private function registerRepositories(): void
+    {
+        $this->app->singleton(HCCityRepository::class);
+    }
 
 }
 
