9f604aba853fde6b05c357c1ead6f65e30bd3283
vytautas <vytautas.rimeikis@gmail.com>
2017-12-08 12:13:30 +0200

    HCCityrepository

---
+++ src/app/Repositories/HCCityRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace interactivesolutions\honeycombregions\app\Repositories;
+
+
+use InteractiveSolutions\HoneycombCore\Repositories\Repository;
+use interactivesolutions\honeycombregions\app\models\regions\HCCities;
+
+/**
+ * Class HCCityRepository
+ * @package interactivesolutions\honeycombregions\app\Repositories
+ */
+class HCCityRepository extends Repository
+{
+
+    /**
+     * @return string
+     */
+    public function model(): string
+    {
+        return HCCities::class;
+    }
+}
